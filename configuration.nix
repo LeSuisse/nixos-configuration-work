c5b93c9bf11540af84f80a35a3ca46e5d14fa63e
Thomas Gerbet <thomas.gerbet@enalean.com>
2023-09-28 09:25:43 +0200

    Persist Tailscale configuration

--- configuration.nix
+++ configuration.nix
@@ -159,6 +159,7 @@
     "L /var/lib/NetworkManager/secret_key - - - - /persist/var/lib/NetworkManager/secret_key"
     "L /var/lib/NetworkManager/seen-bssids - - - - /persist/var/lib/NetworkManager/seen-bssids"
     "L /var/lib/NetworkManager/timestamps - - - - /persist/var/lib/NetworkManager/timestamps"
+    "L /var/lib/tailscale - - - - /persist/var/lib/tailscale"
   ];
   security.sudo.extraConfig = ''
     Defaults lecture = never
