cca5ef76f8a0c2f39b6ecf8ac7056ffdaa9615e4
Thomas Gerbet <thomas.gerbet@enalean.com>
2025-08-27 22:26:58 +0200

    Add OSQuery

--- configuration.nix
+++ configuration.nix
@@ -186,6 +186,31 @@
   };
   systemd.services.tailscaled.serviceConfig.BindPaths = "/persist/var/lib/tailscale:/var/lib/tailscale";
 
+  services.osquery = {
+    enable = true;
+    flags = {
+      enroll_secret_path = "/persist/var/lib/osquery/enroll_secret";
+      tls_hostname = "laptops.fleetdm.enalean.com:443";
+      tls_server_certs = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+      enroll_tls_endpoint = "/api/v1/osquery/enroll";
+      config_plugin = "tls";
+      config_tls_endpoint = "/api/v1/osquery/config";
+      config_refresh = "0";
+      disable_distributed = "false";
+      distributed_plugin = "tls";
+      distributed_tls_max_attempts = "10";
+      distributed_tls_read_endpoint = "/api/v1/osquery/distributed/read";
+      distributed_tls_write_endpoint = "/api/v1/osquery/distributed/write";
+      logger_plugin = "tls";
+      logger_tls_endpoint = "/api/v1/osquery/log";
+      host-identifier = "uuid";
+      enable_syslog = "false";
+      disable_events = "false";
+      enable_bpf_events = "true";
+    };
+  };
+  systemd.services.osqueryd.serviceConfig.BindPaths = "/persist/var/lib/osquery:/var/lib/osquery";
+
   system.stateVersion = "25.05";
 
   nix = {
